1c9b11846008cac7e246d0f95d1c5523a6b9522d
Lionel Mason <capitalcookhouse@gmail.com>
2023-01-16 14:10:32 -0500

    commit tree 85ac45473bf3c393a0d7afb6b6194ad835b35c27

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+
+    #POST /login
+    def create
+        user = User.find_by(username: params[:username])
+        session[:user_id] = user.id
+        render json: user
+    end
+
+    #DELETE /logout
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+# def create
+#     user = User.find_by(username: params[:username])
+#     session[:user_id] = user.id
+#     render json: user
+#   end
+
+#   def destroy
+#     session.delete :user_id
+#     head :no_content
+#   end
+
+end 
